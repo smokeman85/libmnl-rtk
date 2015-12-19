dd857269443cdb233ae2a0ca8c8a0a7006c8edea
Dmitry Pichugin <smokeman85@gmail.com>
2015-12-19 20:45:05 +0300

    Added nlmsghdr, nlattr structs and attr type enum

--- private/ffi/data-struct.rkt
+++ private/ffi/data-struct.rkt
@@ -15,3 +15,26 @@
                                   [buflen _size]
                                   [cur _pointer]
                                   [overflow _bool]))
+
+(define mnl-attr-data-type (_enum '( MNL_TYPE_UNSPEC = 0
+                                     MNL_TYPE_U8
+                                     MNL_TYPE_U16
+                                     MNL_TYPE_U32
+                                     MNL_TYPE_U64
+                                     MNL_TYPE_STRING
+                                     MNL_TYPE_FLAG
+                                     MNL_TYPE_MSECS
+                                     MNL_TYPE_NESTED
+                                     MNL_TYPE_NESTED_COMPAT
+                                     MNL_TYPE_NUL_STRING
+                                     MNL_TYPE_BINARY
+                                     MNL_TYPE_MAX)))
+
+(define-cstruct _nlattr ([nla-len  _uint16]
+                        [nla-type _uint16]))
+
+(define-cstruct _nlmsghdr ([nlmsg-len   _uint32]
+                           [nlmsg-type  _uint16]
+                           [nlmsg-flags _uint16]
+                           [nlmsg-seq   _uint32]
+                           [nlmsg-pid   _uint32]))
