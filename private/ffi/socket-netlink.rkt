5ebf4b26815183ddab277fbbddfddb3b6ad18015
Dmitry Pichugin <smokeman85@gmail.com>
2015-12-21 14:47:38 +0300

    Fix mnl_socket_bind prototype

--- private/ffi/socket-netlink.rkt
+++ private/ffi/socket-netlink.rkt
@@ -14,7 +14,7 @@
 (define-libmnl mnl_socket_open (_fun _int -> _mnl-socket-pointer))
 
 ;int mnl_socket_bind (struct mnl_socket *nl, unsigned int groups, pid_t pid)
-(define-libmnl mnl_socket_bind (_fun _mnl-socket-pointer _uint _int -> _mnl-socket-pointer))
+(define-libmnl mnl_socket_bind (_fun _mnl-socket-pointer _uint _int -> _int))
 
 ;ssize_t mnl_socket_sendto (const struct mnl_socket *nl, const void *buf, size_t len)
 (define-libmnl mnl_socket_sendto (_fun _mnl-socket-pointer _pointer _size -> _ssize))
@@ -30,3 +30,5 @@
 
 ;int mnl_socket_getsockopt (const struct mnl_socket *nl, int type, void *buf, socklen_t *len)
 (define-libmnl mnl_socket_getsockopt (_fun _mnl-socket-pointer _int _pointer _pointer -> _int))
+
+(provide (all-defined-out))
